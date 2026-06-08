a8d9a2875596016ec94c4663328a2b113f83639f
nhammen-kinkaid <nathaniel.hammen@kinkaid.org>
2026-06-08 16:12:15 -0500

    Initial commit

---
+++ conditionals.py
@@ -0,0 +1,11 @@
+# a first look at conditional statements
+
+myVar=51
+print(myVar)
+if myVar>0:
+    print("myVar is positive")
+
+if myVar<0:
+    print("myVar is negative")
+print("The value of myVar is",myVar)
+# what happens if we indent the last line?
