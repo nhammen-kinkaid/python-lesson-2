4f890ffb001b37d8886fa5bd9baa4d13adff6f2f
nhammen-kinkaid <nathaniel.hammen@kinkaid.org>
2026-06-09 15:52:18 -0500

    Create nested.py

---
+++ nested.py
@@ -0,0 +1,10 @@
+myVar=15
+if mVar>0:
+  print("myVar is positive")
+  if myVar>20:
+    print("inner statement 1")
+  print("between inner conditions")
+  if myVar<20:
+    print("inner statement 2")
+  print("end of outer condition")
+print("after outer condition")
